960d33a00c2806ec9fec56de081f5531a7581821
AlinaArakelyan <alinaarakelyan90@gmail.com>
2020-01-29 15:46:29 -0500

    removed list, added advice reducer for redux

---
+++ src/redux/adviceReducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+    advice: []
+}
+
+const adviceReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "SET_ADVICE":
+            return {
+                ...state,
+                advice: action.payload
+            }
+        case "UPDATE_ADVICE":
+            return {
+                ...state,
+                advice: action.payload
+            }
+        
+        default:
+            return state
+    }
+}
+
+export default adviceReducer
